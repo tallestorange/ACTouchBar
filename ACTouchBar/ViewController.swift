47986de89c3cf5e8b7a32c2f9d95227cfab3f1cf
tallestorange <tallestorangeyuhei@gmail.com>
2019-02-06 02:37:58 +0900

    1st Commit

---
+++ ACTouchBar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ACTouchBar
+//
+//  Created by Yuhel Tanaka on 2019/02/06.
+//  Copyright © 2019 Yuhel Tanaka. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
