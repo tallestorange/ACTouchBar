39abd311f2503ae67170c01925f3f8b29293bb70
Yuhel Tanaka <yuheltanaka@YuhelnoMacBook-puro.local>
2019-02-06 23:29:39 +0900

    Backボタンを固定式にした

--- ACTouchBar/UserStatusBarController.swift
+++ ACTouchBar/UserStatusBarController.swift
@@ -16,4 +16,14 @@ class UserStatusBarController: NSCustomTouchBarItem {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    func makeScrollView(items: [NSCustomTouchBarItem]) -> NSScrollView {
+        let views = items.compactMap { $0.view }
+        let stackView = NSStackView(views: views)
+        stackView.spacing = 2
+        stackView.orientation = .horizontal
+        let scrollView = NSScrollView(frame: CGRect(origin: .zero, size: stackView.fittingSize))
+        scrollView.documentView = stackView
+        return scrollView
+    }
 }
